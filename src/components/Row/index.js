e20b06dcf3641afcd27984b018171f3431fe5e1c
Jonny K <Kempj2.jk@gmail.com>
2020-11-05 09:33:19 -0800

    listener attached to the page scroll for a sticky header on table, but I need to figure out how to get it back when you scroll back up. Tabling for now to work on core functionality

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,3 +1,4 @@
+import './style.css'
 
 function Row(props) {
     const e = props.employee
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,30 +1,46 @@
 import React from 'react'
 import Row from '../Row'
 import axios from 'axios'
+import './style.css'
 
 class Table extends React.Component{
     state = {
-        employees:[]
+        employees:[],
+        sticky: false
     }
     componentDidMount= () =>{
         axios.get("https://randomuser.me/api/?results=200&nat=us")
         .then(res => {
             this.setState({employees:res.data.results})
         })
+        
+        window.addEventListener('scroll', this.handleScroll);
     }
 
     buildRow =() =>{
         return this.state.employees.map(e => <Row key={e.id.value} employee={e} />)
     }
-
+    handleScroll = () =>{
+        // let headerPos = document.querySelector(".header").getBoundingClientRect()
+        // if(headerPos.y <= 0){
+        //     this.setState({sticky:true})
+        // }else if(headerPos.y > 0){
+        //     this.setState({sticky:false})
+        // }
+        // // if(headerPos){
+        // //     if(headerPos <=0){
+        // //         console.log(headerPos)
+        // //     }
+        // //     (headerPos <= 0) ? this.setState({sticky:true}) : this.setState({sticky:false}) 
+        // // }
+    }
     render(){
         return (
         <div className="app">
-            
             <h1 id='title'>React Dynamic Table</h1>
-            <table id='students'>
+            <table id='employees'>
                 <tbody>
-                <tr>
+                <tr className={this.state.sticky ? "header sticky" : "header"}>
                     <th>Picture</th>
                     <th>name</th>
                     <th>age</th>
