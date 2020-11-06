62fa04aec1044b67dabc330649650e4243d3a2f0
Jonny K <Kempj2.jk@gmail.com>
2020-11-05 21:59:59 -0800

    changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-//initializes the whole process 
+//initializes the whole process
+// something new 
 ReactDOM.render( <App className="app"/>, document.getElementById('root'));
 
